00001c7904a93c1989c03c1605bc0420e60df44b
Denis Ignatov <dsignatov@licard.com>
2023-03-04 20:20:35 +0300

    Init commit

---
+++ src/main/java/github/denisspec989/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package github.denisspec989.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
